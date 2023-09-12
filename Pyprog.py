5b823d19eef42dd5ee31abe059a81d4e4d6ca3c0
EC2 Default User <ec2-user@ip-172-31-34-251.ap-northeast-1.compute.internal>
2023-09-12 15:16:06 +0000

    V2

--- Pyprog.py
+++ Pyprog.py
@@ -1,21 +1,24 @@
-class Solution:
-    def toGoatLatin(self, sentence):
-        def is_vowel(char):
-            return char.lower() in ['a', 'e', 'i', 'o', 'u']
-
-        words = sentence.split()
-        result = []
-
-        for i, word in enumerate(words, 1):
-            if is_vowel(word[0]):
-                result.append(word + "ma" + "a" * i)
-            else:
-                result.append(word[1:] + word[0] + "ma" + "a" * i)
-
-        return " ".join(result)
-
-solution = Solution()
-result = solution.toGoatLatin("I love Python")
-print(result)
-
-
+##Manoj Baddela##Assignment2#determine if the input string is valid#
+import re
+def isValid(s: str) -> bool:
+    if (s == ''):
+        return True
+    elif not ((s.count('(') - s.count(')')) == 0 and (s.count('[') - s.count(']')) == 0 and (s.count('{') - s.count('}')) == 0):
+        return False
+    else:
+        _result = [re.search(pattern, s)
+                   for pattern in ['\((.)*\)', '\[(.)*\]', '\{(.)*\}']]
+        _result = ['' if _result[i] is None else _result[i].group()[1:-1]
+                   for i in range(len(_result))]
+        return isValid(_result[0]) and isValid(_result[1]) and isValid(_result[2])
+if __name__ == '__main__':
+    #TestCase
+    #True
+    print("Manoj Baddela  Test Cases")
+    print(isValid('[]'))
+    print(isValid('{[]}'))
+    #False
+    print(isValid('([]){'))
+    print(isValid('(]'))
+    print(isValid('([)]'))
+    print(isValid('({()}{[}])'))
