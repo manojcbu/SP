e8761f7193f9397e5d619f9e0951063d2c44ec53
manojcbu <128085998+manojcbu@users.noreply.github.com>
2023-09-11 19:16:35 -0500

    Create Pyprog.py

---
+++ Pyprog.py
@@ -0,0 +1,20 @@
+class Solution:
+    def toGoatLatin(self, sentence):
+        def is_vowel(char):
+            return char.lower() in ['a', 'e', 'i', 'o', 'u']
+
+        words = sentence.split()
+        result = []
+
+        for i, word in enumerate(words, 1):
+            if is_vowel(word[0]):
+                result.append(word + "ma" + "a" * i)
+            else:
+                result.append(word[1:] + word[0] + "ma" + "a" * i)
+
+        return " ".join(result)
+
+solution = Solution()
+result = solution.toGoatLatin("I love Python")
+print(result)
+
